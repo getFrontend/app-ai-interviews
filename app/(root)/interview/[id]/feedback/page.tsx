c2cc944627af3965d907a59b9c3d1623e174bfbd
getFrontend <sergey10goals@gmail.com>
2025-04-04 21:26:33 +0300

    improve Feedback page

--- app/(root)/interview/[id]/feedback/page.tsx
+++ app/(root)/interview/[id]/feedback/page.tsx
@@ -7,8 +7,8 @@ import {
   getFeedbackByInterviewId,
   getInterviewById,
 } from "@/lib/actions/general.action";
-import { Button } from "@/components/ui/button";
 import { getCurrentUser } from "@/lib/actions/auth.action";
+import { MetalButton } from "@/components/MetalButton";
 
 const Feedback = async ({ params }: RouteParams) => {
   const { id } = await params;
@@ -23,22 +23,22 @@ const Feedback = async ({ params }: RouteParams) => {
   });
 
   return (
-    <section className="section-feedback">
+    <section className="section-feedback bg-stone-900/50 px-4 lg:px-8 py-3 lg:py-8 rounded-3xl">
       <div className="flex flex-row justify-center">
         <h1 className="text-4xl font-semibold">
-          Feedback on the Interview -{" "}
-          <span className="capitalize">{interview.role}</span> Interview
+          Feedback:{" "}
+          <span className="capitalize">{interview.role}</span> interview
         </h1>
       </div>
 
-      <div className="flex flex-row justify-center ">
-        <div className="flex flex-row gap-5">
+      <div className="flex flex-row justify-center">
+        <div className="flex flex-row gap-5 max-sm:flex-col max-sm:items-center">
           {/* Overall Impression */}
           <div className="flex flex-row gap-2 items-center">
-            <Image src="/star.svg" width={22} height={22} alt="star" />
+            <Image src="/star-2.svg" width={22} height={22} alt="star" />
             <p>
               Overall Impression:{" "}
-              <span className="text-primary-200 font-bold">
+              <span className="text-orange-300 font-bold">
                 {feedback?.totalScore}
               </span>
               /100
@@ -57,16 +57,14 @@ const Feedback = async ({ params }: RouteParams) => {
         </div>
       </div>
 
-      <hr />
-
       <p>{feedback?.finalAssessment}</p>
 
       {/* Interview Breakdown */}
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-5">
         <h2>Breakdown of the Interview:</h2>
         {feedback?.categoryScores?.map((category, index) => (
-          <div key={index}>
-            <p className="font-bold">
+          <div key={index} className="md:ml-4">
+            <p className="text-primary-200 font-bold">
               {index + 1}. {category.name} ({category.score}/100)
             </p>
             <p>{category.comment}</p>
@@ -75,8 +73,8 @@ const Feedback = async ({ params }: RouteParams) => {
       </div>
 
       <div className="flex flex-col gap-3">
-        <h3>Strengths</h3>
-        <ul>
+        <h3>Strengths:</h3>
+        <ul className="md:ml-6">
           {feedback?.strengths?.map((strength, index) => (
             <li key={index}>{strength}</li>
           ))}
@@ -84,33 +82,34 @@ const Feedback = async ({ params }: RouteParams) => {
       </div>
 
       <div className="flex flex-col gap-3">
-        <h3>Areas for Improvement</h3>
-        <ul>
+        <h3>Areas for Improvement:</h3>
+        <ul className="md:ml-6">
           {feedback?.areasForImprovement?.map((area, index) => (
             <li key={index}>{area}</li>
           ))}
         </ul>
       </div>
 
-      <div className="buttons">
-        <Button className="btn-secondary flex-1">
-          <Link href="/" className="flex w-full justify-center">
-            <p className="text-sm font-semibold text-primary-200 text-center">
+      <div className="buttons mt-4">
+          <Link href="/" className="flex w-full justify-center lg:justify-end items-center">
+            <MetalButton 
+              className="text-md px-8"
+              variant="primary"
+            >
               Back to dashboard
-            </p>
+            </MetalButton>
           </Link>
-        </Button>
-
-        <Button className="btn-primary flex-1">
           <Link
             href={`/interview/${id}`}
-            className="flex w-full justify-center"
+            className="flex w-full justify-center lg:justify-start items-center"
           >
-            <p className="text-sm font-semibold text-black text-center">
+            <MetalButton 
+              className="text-md px-8"
+              variant="bronze"
+            >
               Retake Interview
-            </p>
+            </MetalButton>
           </Link>
-        </Button>
       </div>
     </section>
   );
